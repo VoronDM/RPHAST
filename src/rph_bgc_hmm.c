8c27038babcdae9b1d53e725be7ea0a9213bbb4e
Vitaly Korchagin <vitaly_korchagin@rambler.ru>
2026-09-14 17:08:57 +0300

    Update rph_bgc_hmm.c

--- src/rph_bgc_hmm.c
+++ src/rph_bgc_hmm.c
@@ -20,6 +20,11 @@ Last updated: 4/21/2010
 #include <Rdefines.h>
 #include <R_ext/Random.h>
 
+/* Fix for R 4.0+ where EXTPTR_PTR was removed */
+#ifndef EXTPTR_PTR
+#define EXTPTR_PTR(x) R_ExternalPtrAddr(x)
+#endif
+
 SEXP rph_listOfLists_to_SEXP(ListOfLists *lol);
 
 //many more options to be added

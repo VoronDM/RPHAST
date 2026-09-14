d0429b43c5122676a37b04c866283fe760237926
Vitaly Korchagin <vitaly_korchagin@rambler.ru>
2026-09-14 16:45:28 +0300

    Update rph_phyloFit.c

--- src/rph_phyloFit.c
+++ src/rph_phyloFit.c
@@ -29,6 +29,11 @@ Last updated: 1/5/2010
 #include <Rdefines.h>
 #include <R_ext/Random.h>
 
+/* Fix for R 4.0+ where EXTPTR_PTR was removed */
+#ifndef EXTPTR_PTR
+#define EXTPTR_PTR(x) R_ExternalPtrAddr(x)
+#endif
+
 TreeNode* rph_tree_new(SEXP treeStr);
 subst_mod_type rph_get_subst_mod(SEXP mod);
 
